74263dba75f63a1b8d8a8b63cba882eacf4f42ef
Sabine Ingabire-test <ingabirezizas@gmail.com>
2025-06-09 12:35:23 +0200

    positive Sum

--- katas.js
+++ katas.js
@@ -1,10 +1,13 @@
 
-function evenOrOdd(number) {
-  if(number%2 == 0) {
-    return "Even";
-  } else if (number%2 !== 0){
-    return "Odd";
+function positiveSum(arr) {
+  sum =0;
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] >= 0){
+      sum += arr[i];
+      }
+    
   }
+  return sum;
   
 }
-evenOrOdd(6);
+console.log(positiveSum([1,-4,12]));
