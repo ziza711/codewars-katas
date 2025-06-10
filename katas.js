9f5f2ab488d5052af5c6f70976f08dae606c5b2f
Sabine Ingabire-test <ingabirezizas@gmail.com>
2025-06-10 10:08:37 +0200

    Basic Mathematical Operations

--- katas.js
+++ katas.js
@@ -1,13 +1,18 @@
 
-function positiveSum(arr) {
-  sum =0;
-  for(let i=0; i<arr.length; i++){
-    if(arr[i] >= 0){
-      sum += arr[i];
-      }
-    
+function basicOp(operation, value1, value2){
+  //Code
+  switch (operation){
+    case'+':
+     return value1 + value2;
+    case'-':
+     return value1 - value2
+    case'*':
+     return value1 * value2
+    case'/':
+     return value1 / value2
+    default:
+    return "invalid operation"
   }
-  return sum;
   
 }
-console.log(positiveSum([1,-4,12]));
+  console.log(basicOp('/',49,7));
