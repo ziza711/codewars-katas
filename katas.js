59705c73b9d149a8d18c3cb56147ee2ae3d49a4d
Sabine Ingabire-test <ingabirezizas@gmail.com>
2025-06-17 11:37:46 +0200

    Switch it Up

--- katas.js
+++ katas.js
@@ -1,11 +1,40 @@
 
-function squareSum(numbers){
-  let sum = 0;
-  for (let i=0; i<numbers.length; i++){
-   sum += numbers[i] ** 2;
-       
- }
-return sum;
-
+function switchItUp(number){
+//Write your own Code!
+  switch(number){
+      
+    case 0:
+      return 'Zero'
+      break;
+    case 1:
+      return 'One'
+      break;
+    case 2:
+      return 'Two'
+      break;
+   case 3:
+      return 'Three'
+      break;
+     case 4:
+      return 'Four'
+      break;
+   case 5:
+      return 'Five'
+      break;
+   case 6:
+      return 'Six'
+      break;
+   case 7:
+      return 'Seven'
+      break;
+   case 8:
+      return 'Eight'
+      break;
+   case 9:
+      return 'Nine'
+      break;
+  default:
+  return 'not valid';
 }
-console.log(squareSum([1,3,2]))
+}
+console.log(switchItUp(5));
