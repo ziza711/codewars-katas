2290dc328d27dccdb7933fd0b8d4335e679032c7
Sabine Ingabire-test <ingabirezizas@gmail.com>
2025-06-23 11:34:46 +0200

    what is between

--- katas.js
+++ katas.js
@@ -1,40 +1,11 @@
 
-function switchItUp(number){
-//Write your own Code!
-  switch(number){
-      
-    case 0:
-      return 'Zero'
-      break;
-    case 1:
-      return 'One'
-      break;
-    case 2:
-      return 'Two'
-      break;
-   case 3:
-      return 'Three'
-      break;
-     case 4:
-      return 'Four'
-      break;
-   case 5:
-      return 'Five'
-      break;
-   case 6:
-      return 'Six'
-      break;
-   case 7:
-      return 'Seven'
-      break;
-   case 8:
-      return 'Eight'
-      break;
-   case 9:
-      return 'Nine'
-      break;
-  default:
-  return 'not valid';
+function between(a, b) {
+  const result = [];
+  for (let i = a ; i <= b; i++) {
+    result.push(i);
+  }
+  return result;
+
+
 }
-}
-console.log(switchItUp(5));
+console.log(between(-2,4));
