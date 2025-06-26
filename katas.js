a7cee5f018d732f2ad4f635d4efe4f5c4cf90ed9
Sabine Ingabire-test <ingabirezizas@gmail.com>
2025-06-26 10:54:30 +0200

    Parse nice int from char problem

--- katas.js
+++ katas.js
@@ -1,6 +1,19 @@
-function makeUpperCase(str) {
-  // Code here
-  const word = str.toUpperCase()
-  return word;
+function getAge(inputString){
+// return the girl's correct age as an integer. Happy coding :)
+  for(let i=0; i<inputString.length; i++){
+    const char=inputString[i];
+  if(char > '0' && char <= '9'){
+    return Number(char)
+  }
+    }
+  return 0;
 }
-console.log(makeUpperCase('sabine'))
+console.log(getAge('4 years old'))
+
+        Best Practices0
+        Clever0
+    0
+    Fork
+    Compare with your solution
+    Link
+
